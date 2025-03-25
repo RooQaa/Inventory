638ffcdcec28d293df154199f7b0ae3faffff821
yasmine <yasminee455@gmail.com>
2025-03-26 01:15:17 +0200

    file setup and organization

--- client/src/Components/Dashboards/DashboardAdmin.jsx
+++ client/src/Components/Dashboards/DashboardAdmin.jsx
@@ -1,12 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const DashboardAdmin = () => {
   return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>لوحة تحكم المشرف</h1>
-      <p>مرحبًا بك في لوحة التحكم الخاصة بك</p>
-    </div>
-  );
-};
+    <div>DashboardAdmin</div>
+  )
+}
 
-export default DashboardAdmin;
+export default DashboardAdmin
